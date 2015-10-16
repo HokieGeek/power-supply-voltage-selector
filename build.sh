4a2dd362996f618284e99a50cbb660e63f84da25
HokieGeek <andres.f.perez@gmail.com>
2015-10-15 23:08:27 -0400

    Removing possibly redundant scripts

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-here=$(cd $(dirname $0); pwd)
-src=${1}
-bin=`echo ${src} | cut -d'.' -f1`".hex"
-
-${here}/compile.sh ${src} && sudo ${here}/upload.sh ${bin}
--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-bin=${1}
-chip=`echo ${bin} | cut -d'/' -f1 | tr '[:upper:]' '[:lower:]'`
-
-echo "Chip: ${chip}"
-echo "Binary: ${bin}"
-
-avrdude -p ${chip} -P usb -c usbtiny -U flash:w:${bin}
