b5459433a4d1217909811caa285bb2238520e510
HokieGeek <andres.f.perez@gmail.com>
2015-10-16 10:52:39 -0400

    Adding code for using the AVR SPI interface

--- spi.c
+++ spi.c
@@ -15,12 +15,10 @@ void SetChipSelectLow(SpiDevice *const dev) {
 
 void ToggleSerialClock(SpiDevice *const dev) {
     PORTB |= (1<<dev->serialClock);
-    _delay_ms(3);
     PORTB &= ~(1<<dev->serialClock);
-    _delay_ms(3);
 }
 
-void WriteBytes(SpiDevice *const dev, int numBytes, uint8_t data[]) {
+void SpiWriteBytes(SpiDevice *const dev, int numBytes, uint8_t data[]) {
     SetChipSelectLow(dev);
     for (int byte = 0; byte < numBytes; byte++) {
         for (uint8_t i = 0; i < 8; i++) {
@@ -48,3 +46,21 @@ SpiDevice *const Init3WireSpiDevice(int chipSelect, int serialClock, int serialD
 
     return dev;
 }
+
+/*
+// While this is pretty great, the attiny85's registers are 8-bit so 
+// I can't use it to set the MCP41010's 16-bit registers. Still need to do bit-banging.
+void InitSpiMaster(void) {
+    // Set SCK and MOSI as output
+    DDRB |= (1<<PB2)|(1<<PB0)
+    
+    // Enable SPI as master, but what is SPR0?
+    SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+}
+
+void SpiSend(uint8_t data) {
+    SPDR = data;
+    
+    while(!(SPSR & (1<<SPIF)));
+}
+*/
