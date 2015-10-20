41028295f2a6ace1694638cdf96746fbb7e6cd8f
HokieGeek <andres.f.perez@gmail.com>
2015-10-20 12:22:19 -0400

    Renabling the USI code I had

--- spi.c
+++ spi.c
@@ -52,18 +52,9 @@ SpiDevice *const InitSoftwareSpi(int chipSelect, int serialClock, int serialData
 // While this is pretty great, the attiny85's registers are 8-bit so 
 // I can't use it to set the MCP41010's 16-bit registers. Still need to do bit-banging.
 
-/*
-void SetChipSelectHigh2(SpiDevice2 *const dev) {
-    PORTB |= (1<<dev->chipSelect);
-}
-
-void SetChipSelectLow2(SpiDevice2 *const dev) {
-    PORTB &= ~(1<<dev->chipSelect);
-}
-
-SpiDevice2 *const InitSpiMaster(int chipSelect) {
-    const int sdsize = sizeof(SpiDevice2);
-    SpiDevice2 *const dev = (SpiDevice2*)malloc(sdsize);
+SpiDevice *const InitSpiMaster(int chipSelect) {
+    const int sdsize = sizeof(SpiDevice);
+    SpiDevice *const dev = (SpiDevice*)malloc(sdsize);
     dev->chipSelect = chipSelect;
 
     USICR &= ~((1<<USISIE)|(1<<USIOIE)|(1<<USIWM1));
@@ -72,7 +63,7 @@ SpiDevice2 *const InitSpiMaster(int chipSelect) {
     // Set SCK and MOSI as output
     DDRB |= (1<<PB2)|(1<<PB0)|(1<<chipSelect);
 
-    SetChipSelectHigh2(dev);
+    SetChipSelectHigh(dev);
 
     return dev;
 }
@@ -81,31 +72,37 @@ uint8_t spiSend(uint8_t data) {
     USIDR = data;
     USISR = (1<<USIOIF);
 
-    asm volatile("nop");
+    // asm volatile("nop");
     while (!(USISR & (1<<USIOIF))) USICR |= (1<<USITC);
 
     return USIDR;
 }
 
-uint8_t SpiSend(SpiDevice2 *const dev, uint8_t data) {
-    SetChipSelectLow2(dev);
+uint8_t SpiSend(SpiDevice *const dev, uint8_t data) {
+    SetChipSelectLow(dev);
 
     uint8_t ret = spiSend(data);
 
-    SetChipSelectHigh2(dev);
+    SetChipSelectHigh(dev);
 
     return ret;
 }
 
-uint8_t SpiSend16(SpiDevice2 *const dev, uint8_t byte1, uint8_t byte2) {
-    SetChipSelectLow2(dev);
+uint8_t SpiSend16(SpiDevice *const dev, uint8_t byte1, uint8_t byte2) {
+    SetChipSelectLow(dev);
 
     uint8_t ret = spiSend(byte1);
     ret = spiSend(byte2);
 
-    SetChipSelectHigh2(dev);
+    SetChipSelectHigh(dev);
     _delay_ms(500);
 
     return ret;
+}
+
+/*
+uint16_t SpiSend16(SpiDevice *const dev, uint16_t bytes) {
+    // TODO: see how the Arduino SPI.h library does it
+    return 0xFFFF;
 }
 */
